332608525635851c3f4f4d08f6300d277b026833
David Plomar <david.palomar@servexternos.gruposantander.es>
2020-02-18 23:50:02 +0100

    Solution commit

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,149 +1,65 @@
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
+/**
+ * @author david palomar delgado
+ */
 public class Solution {
+    /**
+     * Checks for array leaders
+     * @param K range size
+     * @param M Maximum integer
+     * @param A Initial array
+     * @return the array with leaders without repetition
+     */
     public int[] solution(int K, int M, int[] A){
 
         int n = A.length;
-
-        List<Integer> lista = new ArrayList<>();
-        List<Integer> numbers = arrayToList(A);
-
-
-
-        for (int i=0; i < A.length; i += K){
-
-            final List<Integer> rango = numbers.subList(i, K );
-            final List<Integer> rest = numbers.subList(K, numbers.size());
-            rango.replaceAll(value->value+1);
-            final ArrayList<Integer> temp = new ArrayList<>(rango);
-            temp.addAll(rest);
-
-//            lista.addAll(getRange(K, numbers, i));
-
-            final List<Integer> ocurrences = getOcurrences(n, temp);
-        }
-//        final Set<Integer> plainOcurrences = getOcurrences(n, numbers);
-        final List<Integer> rangeOcurrences = getOcurrences(n, lista);
-
         SortedSet<Integer> sortedSet = new TreeSet<>();
-//        sortedSet.addAll(plainOcurrences);
-        sortedSet.addAll(rangeOcurrences);
+
+        for (int i=0; i < n; i += K){
+            int[] B = new int[n];
+            if (n < i+K) {
+                K = n-i;
+            }
+            System.arraycopy(A,i,B,i,K);
+
+            int[] rango = Arrays.stream(B).map(c -> c!=0 ? c+ 1: c).toArray();
+
+            for (int j = 0; j < n; j++) {
+                B[j] = A[j];
+                if (rango[j] != 0) {
+                    B[j] = rango[j];
+                }
+            }
+            sortedSet.addAll(getOcurrences(n, arrayToList(B)));
+        }
         return sortedSet.stream().mapToInt(i -> i).toArray();
 
     }
 
+    /**
+     * Find the numbers that are repeated more than half of array size
+     * @param size initial array size
+     * @param lista the array to looks for the numbers
+     * @return the numbers
+     */
     private List<Integer> getOcurrences(int size, List<Integer> lista) {
-        final List<Integer> collect = lista.stream()
-                .filter(i -> Collections.frequency(lista, i) >= size / 2)
+        return  lista.stream()
+                .filter(i -> Collections.frequency(lista, i) > size / 2)
                 .collect(Collectors.toList());
-//        collect.replaceAll(value -> value - 1);
-        return collect;
-
-//        final Set<Integer> collect1 = collect.stream()
-//                .filter(i -> Collections.frequency(collect, i) > 1)
-//                .collect(Collectors.toSet());
-//        return collect1;
     }
 
+    /**
+     * Utility method to convert an array to a List of wrappers
+     * @param A the array to convert
+     * @return the Wrapper List
+     */
     private List<Integer> arrayToList(int[] A) {
         return IntStream.of(A)
                 .boxed()
                 .collect(Collectors.toList());
     }
-
-    private List<Integer> getRange(int K, List<Integer> numbers, int i) {
-        return Stream.of(
-                        Arrays.stream(Arrays.copyOfRange(numbers.toArray(), i, K + i))
-                                .filter(Objects::nonNull)
-                                .map(Integer.class::cast)
-                                .collect(Collectors.toList())
-
-                ).map((rango -> {
-                    if (rango.size() == K) {
-                        rango.replaceAll(value -> value + 1);
-                    }
-                    return rango;
-                })).flatMap(x -> x.stream())
-                        .collect(Collectors.toList());
-    }
-
-    public int[] solution2(final int K, final int M, final int[] A) {
-        final int size = A.length;
-        final int enough = size/2 + 1;
-
-        int leader1 = -1;
-        int leader2 = -1;
-
-        final int[] occurrences = new int[M + 2];
-        for (int i = 0; i < size; i++) {
-            occurrences[A[i]]++;
-        }
-
-        // incrementing first segment of K elements
-        for (int i = 0; i < K; i++) {
-            occurrences[A[i]]--;
-            occurrences[++A[i]]++;
-        }
-
-        // checking if a leader exists already
-        for (int i = 0; i < M + 2; i++) {
-            if (occurrences[i] >= enough) {
-                leader1 = i;
-                break;
-            }
-        }
-
-        // traversing one by one until reaching the end of
-        // the array or until two unique leaders are found
-        for (int i = 0; i < size - K; i++) {
-            occurrences[A[i]]--;
-            occurrences[--A[i]]++;
-            occurrences[A[i + K]]--;
-            occurrences[++A[i + K]]++;
-
-            final int candidateFirst = A[i];
-            final int candidateLast = A[i + K];
-
-            if (occurrences[candidateLast] >= enough) {
-                if (leader1 == -1) {
-                    leader1 = candidateLast;
-                } else if (candidateLast != leader1) {
-                    leader2 = candidateLast;
-                }
-
-                if (leader2 != -1) {
-                    break;
-                }
-            } else if (occurrences[candidateFirst] >= enough) {
-                if (leader1 == -1) {
-                    leader1 = candidateFirst;
-                } else if (candidateFirst != leader1) {
-                    leader2 = candidateFirst;
-                }
-
-                if (leader2 != -1) {
-                    break;
-                }
-            }
-        }
-        return buildResultArray(leader1, leader2);
-    }
-
-    private int[] buildResultArray(final int leader1, final int leader2) {
-        if (leader1 == -1 && leader2 == -1) {
-            return new int[0];
-        } else if (leader2 == -1) {
-            return new int[]{leader1};
-        } else if (leader1 < leader2) {
-            return new int[]{leader1, leader2};
-        } else {
-            return new int[]{leader2, leader1};
-        }
-    }
-
-
 
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -1,33 +1,29 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class SolutionTest {
+    private Solution sut;
 
-    @Test
-    void si_el_5_se_repite_mas_devuelve_el_5() {
-        int[] numbers = {1,5,5,3,2,2,5};
-        int[] result = {5};
-        Solution sut = new Solution();
-        assertNotNull(sut.solution2(3,5,numbers));
-        assertArrayEquals(sut.solution2(3,5,numbers),result );
-        assertEquals(sut.solution2(3,5,numbers)[0],5);
+    @BeforeEach
+    void setUp(){
+        sut = new Solution();
     }
-
     @Test
-    void test() {
-        Solution sut = new Solution();
+    void given_an_non_valid_input_should_return_empty_array() {
         assertArrayEquals(new int[]{}, sut.solution(3, 3, new int[]{1, 2, 3}));
+    }
+    @Test
+    void given_an_only_one_input_should_return_only_leader() {
         assertArrayEquals(new int[]{2}, sut.solution(1, 1, new int[]{1}));
-        assertArrayEquals(new int[]{2, 3}, sut.solution2(3, 5, new int[]{2, 3, 3, 3, 2, 2, 3}));
-        assertArrayEquals(new int[]{2, 3}, sut.solution(4, 2, new int[]{1, 2, 2, 1, 2}));
-        assertArrayEquals(new int[]{}, sut.solution(3, 2, new int[]{1, 1, 1, 1, 1, 1}));
-        assertArrayEquals(new int[]{1}, sut.solution(3, 2, new int[]{1, 1, 1, 1, 1, 1, 1}));
-        assertArrayEquals(new int[]{1}, sut.solution(1, 2, new int[]{1, 1, 1, 1, 1, 1, 1}));
-        assertArrayEquals(new int[]{2}, sut.solution(1, 2, new int[]{1}));
-        assertArrayEquals(new int[]{8}, sut.solution(3, 10, new int[]{1, 6, 6, 7, 7, 8, 8, 8}));
-        assertArrayEquals(new int[]{2}, sut.solution(5, 5, new int[]{1, 1, 1, 1, 1, 1, 1, 1, 1}));
-        assertArrayEquals(new int[]{10, 11}, sut.solution(4, 20, new int[]{10, 10, 11, 10, 11, 11, 9, 11, 10}));
-        assertArrayEquals(new int[]{4, 5}, sut.solution(5, 10, new int[]{4, 2, 4, 4, 1, 4, 5, 3, 5, 5, 3}));
+    }
+    @Test
+    void given_an_serie_with_valid_segment_should_return_several_leaders() {
+        assertArrayEquals(new int[]{2, 3}, sut.solution(3, 5, new int[]{2, 2, 3, 1, 2, 2, 3}));
+    }
+    @Test
+    void given_an_serie_with_valid_long_segment_should_return_just_the_leader() {
+          assertArrayEquals(new int[]{5}, sut.solution(5, 10, new int[]{4, 2, 4, 4, 1, 4, 5, 3, 5, 5, 3}));
     }
 }
